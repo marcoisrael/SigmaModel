d9ab8f3ac01548eb5e2c4e8b40368bce7eb4a53e
marcoisrael <marco_israel@ciencias.unam.mx>
2023-10-01 20:43:31 -0600

    3nd test

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #estas son las instrucciones para compilar y ejecutar el programa 
 #el compilador es gfortran
-gfortran -ofast -floop-parallelize-all -o sigma-model fortran/m_progress_bar.f90 fortran/functions.f90 fortran/method.f90 fortran/sigma-model.f90
-./sigma-model
+gfortran -ofast -floop-parallelize-all -o main fortran/m_progress_bar.f90 fortran/functions.f90 fortran/algorithm.f90 fortran/main.f90
+./main
