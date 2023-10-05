5a14aaad7e584926d99ee203c640b055f8fad984
marcoisrael <marco_israel@ciencias.unam.mx>
2023-10-05 03:00:08 -0600

    add plot

--- plot.py
+++ plot.py
@@ -1,6 +1,8 @@
 #!/bin/python3
 import numpy as np
 import matplotlib.pyplot as plt
-data = np.loadtxt('output/file/therm.csv')
+data = np.loadtxt('output/data/therm.csv')
 plt.plot(data)
+plt.xlabel('#sweeps')
+plt.ylabel('Energy density')
 plt.savefig('output/plot/therm.png')
