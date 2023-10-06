b17bdcc19b19e50be2ed6ef73de583b18bde58a1
israel <marco_israel@gmail.com>
2023-10-06 16:29:36 -0600

    add plot

--- main.f90
+++ main.f90
@@ -10,14 +10,23 @@ VOLUME = LENGTH*LENGTH
 thermalization = 1e3
 temperature = 4
 beta = 1/temperature
-cycles = 200
-N = 100
+cycles = 100
+N = 10
 path = "output/data/therm.csv"
 allocate(s(LENGTH,LENGTH,3))
 call hot_start(s)
 open(unit=1, file=trim(path))
 do i=1, cycles
-    write(1,"(f0.16)") system_energy(s)
-    call step_metropolis(s)
+    med(1) = 0
+    var(1) = 0
+    do j=1, N
+        obs(1) = system_energy(s)
+        med(1) = med(1)+obs(1)
+        var(1) = var(1)+obs(1)**2
+        call step_metropolis(s)
+    end do
+    med(1) = med(1)/N
+    var(1) = (var(1)-N*med(1)**2)/(N-1)
+    write(1,"(2(f0.16,:,','))") med(1), sqrt(var(1)/N)
 end do
 end program
--- plot.py
+++ plot.py
@@ -1,8 +1,11 @@
 #!/bin/python3
 import numpy as np
 import matplotlib.pyplot as plt
-data = np.loadtxt('output/data/therm.csv')
-plt.plot(data)
+data = np.loadtxt('output/data/therm.csv', delimiter=',')
+y, yerr = data.transpose()
+sweeps = np.arange(1,y.size+1)
+plt.errorbar(sweeps, y, yerr=yerr, marker='.', ls='')
+plt.plot(sweeps, np.zeros_like(y)+np.mean(y))
 plt.xlabel('#sweeps')
 plt.ylabel('Energy density')
 plt.savefig('output/plot/therm.png')
