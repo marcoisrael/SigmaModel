3742f6c8a6355e824094a2abfb034db8dd8426b2
marcoisrael <marco_israel@ciencias.unam.mx>
2023-10-13 12:36:26 -0600

    last update

--- functions.f90
+++ functions.f90
@@ -1,163 +1,163 @@
 module functions
-real(8) ::  pi=4.0*datan(1.0d0), beta
-real(8), dimension(4) :: values 
-logical :: update_values=.false.
-integer :: LENGTH, VOLUME
-! logical :: save
-contains
+    real(8) ::  pi=4.0*datan(1.0d0), beta
+    real(8), dimension(4) :: values 
+    logical :: update_values=.false.
+    integer :: LENGTH, VOLUME
+    ! logical :: save
+    contains
 
-subroutine hot_start(s)
-    real(8), dimension(LENGTH,LENGTH) :: theta, phi, r
-    real(8), dimension(LENGTH,LENGTH,3) :: s
-    call random_number(r)
-    call random_number(phi)
-    theta = acos(1-2*r)
-    phi = 2*pi*phi
-    s(:,:, 1) = sin(theta)*cos(phi)
-    s(:,:, 2) = sin(theta)*sin(phi)
-    s(:,:, 3) = cos(theta)
-end subroutine
+    subroutine hot_start(s)
+        real(8), dimension(LENGTH,LENGTH) :: theta, phi, r
+        real(8), dimension(LENGTH,LENGTH,3) :: s
+        call random_number(r)
+        call random_number(phi)
+        theta = acos(1-2*r)
+        phi = 2*pi*phi
+        s(:,:, 1) = sin(theta)*cos(phi)
+        s(:,:, 2) = sin(theta)*sin(phi)
+        s(:,:, 3) = cos(theta)
+    end subroutine
 
-function random()
-    real(8) :: random
-    call random_number(random)
-end function
+    function random()
+        real(8) :: random
+        call random_number(random)
+    end function
 
-function random_vector()
-    real(8),dimension(3) :: random_vector
-    real(8) :: theta, phi
-    theta = acos(1-2*random())
-    phi = 2*pi*random()
-    random_vector = [sin(theta)*cos(phi), sin(theta)*sin(phi), cos(theta)]
-end function
+    function random_vector()
+        real(8),dimension(3) :: random_vector
+        real(8) :: theta, phi
+        theta = acos(1-2*random())
+        phi = 2*pi*random()
+        random_vector = [sin(theta)*cos(phi), sin(theta)*sin(phi), cos(theta)]
+    end function
 
-function modl(i)
-    integer i, modL
-    modl = modulo(i, LENGTH)
-    if (modl==0) then
-        modl = LENGTH
-    end if
-end function
-
-function wolff_reflection(v, w)
-    real(8), dimension(3) :: v, w, wolff_reflection
-    wolff_reflection = v-2*dot_product(v, w)*w
-end function
-
-function cross_product(a, b)
-    real(8), dimension(3) :: cross_product
-    real(8), dimension(3) :: a, b
-    cross_product(1) = a(2) * b(3) - a(3) * b(2)
-    cross_product(2) = a(3) * b(1) - a(1) * b(3)
-    cross_product(3) = a(1) * b(2) - a(2) * b(1)
-end function
-
-function system_charge(s)
-    real(8), dimension(LENGTH,LENGTH,3) :: s
-    real(8) system_charge, X1, Y1, X2, Y2
-    real(8),dimension(3) :: e1, e2, e3, e4
-    integer :: k=1
-    system_charge = 0
-    do i=1, LENGTH
-        do j=1, LENGTH
-            e1 = s(modl(i+1),j,:)
-            e2 = s(modl(i+1),modl(j+1),:)
-            e3 = s(i,j,:)
-            e4 = s(i,modl(j+1),:)
-            if (k>0) then    
-                X1 = 1+dot_product(e1,e2)+dot_product(e2,e3)+dot_product(e3,e1)
-                Y1 = dot_product(e1, cross_product(e2,e3))
-                X2 = 1+dot_product(e4,e3)+dot_product(e3,e2)+dot_product(e2,e4)
-                Y2 = dot_product(e4, cross_product(e3,e2))
-            else 
-                X1 = 1+dot_product(e1,e2)+dot_product(e2,e4)+dot_product(e4,e1)
-                Y1 = dot_product(e1,cross_product(e2,e4))
-                X2 = 1+dot_product(e1,e4)+dot_product(e4,e3)+dot_product(e3,e1)
-                Y2 = dot_product(e1,cross_product(e4,e3))
-            end if
-            system_charge = system_charge+atan2(Y1,X1)+atan2(Y2,X2)
-            k =-k
-        end do
-    end do
-    system_charge=0.5*system_charge/pi
-end function
-
-function system_energy(s)
-    real(8),dimension(LENGTH,LENGTH,3) :: s
-    real(8), dimension(3) :: sx, sx_right, sx_down
-    real(8) :: system_energy
-    system_energy = 0
-    do i=1, LENGTH
-        do j=1, LENGTH
-            sx = s(i,j,:)
-            sx_right = s(modl(i+1),j,:)
-            sx_down = s(i,modl(j+1),:)
-            system_energy = system_energy-dot_product(sx,sx_right)-dot_product(sx,sx_down)
-        end do
-    end do
-    system_energy = system_energy/VOLUME
-end function
-
-function is_bond(sx, sy, w)
-    logical :: is_bond
-    real(8), dimension(3) :: sx, sy, w
-    real(8) :: delta
-    delta = -dot_product(wolff_reflection(sx, w), sy)+dot_product(sx, sy)
-    if (random()<=1-exp(min(0.,-beta*delta))) then
-        is_bond = .true.
-    else
-        is_bond = .false.
-    end if
-end function
-
-subroutine join(group, label1, label2, largest_label)
-    integer, dimension(LENGTH,LENGTH) :: group
-    integer :: label1, label2, label_min, label_max
-    if (label1/=label2) then
-        if (label1<label2) then
-            label_max = label2
-            label_min = label1
-        else
-            label_max = label1
-            label_min = label2
+    function modl(i)
+        integer i, modL
+        modl = modulo(i, LENGTH)
+        if (modl==0) then
+            modl = LENGTH
         end if
+    end function
+
+    function wolff_reflection(v, w)
+        real(8), dimension(3) :: v, w, wolff_reflection
+        wolff_reflection = v-2*dot_product(v, w)*w
+    end function
+
+    function cross_product(a, b)
+        real(8), dimension(3) :: cross_product
+        real(8), dimension(3) :: a, b
+        cross_product(1) = a(2) * b(3) - a(3) * b(2)
+        cross_product(2) = a(3) * b(1) - a(1) * b(3)
+        cross_product(3) = a(1) * b(2) - a(2) * b(1)
+    end function
+
+    function system_charge(s)
+        real(8), dimension(LENGTH,LENGTH,3) :: s
+        real(8) system_charge, X1, Y1, X2, Y2
+        real(8),dimension(3) :: e1, e2, e3, e4
+        integer :: k=1
+        system_charge = 0
         do i=1, LENGTH
             do j=1, LENGTH
-                if (group(i,j)==label_max) then
-                    group(i,j) = label_min
-                else if (group(i,j)>label_max) then
-                    group(i,j) = group(i,j)-1
+                e1 = s(modl(i+1),j,:)
+                e2 = s(modl(i+1),modl(j+1),:)
+                e3 = s(i,j,:)
+                e4 = s(i,modl(j+1),:)
+                if (k>0) then    
+                    X1 = 1+dot_product(e1,e2)+dot_product(e2,e3)+dot_product(e3,e1)
+                    Y1 = dot_product(e1, cross_product(e2,e3))
+                    X2 = 1+dot_product(e4,e3)+dot_product(e3,e2)+dot_product(e2,e4)
+                    Y2 = dot_product(e4, cross_product(e3,e2))
+                else 
+                    X1 = 1+dot_product(e1,e2)+dot_product(e2,e4)+dot_product(e4,e1)
+                    Y1 = dot_product(e1,cross_product(e2,e4))
+                    X2 = 1+dot_product(e1,e4)+dot_product(e4,e3)+dot_product(e3,e1)
+                    Y2 = dot_product(e1,cross_product(e4,e3))
                 end if
+                system_charge = system_charge+atan2(Y1,X1)+atan2(Y2,X2)
+                k =-k
             end do
         end do
-        largest_label = largest_label-1
-    end if
-end subroutine
+        system_charge=0.5*system_charge/pi
+    end function
 
-function char_to_int(i)
-    character(20) :: style, char_to_int
-    integer :: i
-    if (i<10) then
-        style = "(I1)"
-    else
-        style = "(I2)"
-    end if
-    write(char_to_int, style) i
-end function
+    function system_energy(s)
+        real(8),dimension(LENGTH,LENGTH,3) :: s
+        real(8), dimension(3) :: sx, sx_right, sx_down
+        real(8) :: system_energy
+        system_energy = 0
+        do i=1, LENGTH
+            do j=1, LENGTH
+                sx = s(i,j,:)
+                sx_right = s(modl(i+1),j,:)
+                sx_down = s(i,modl(j+1),:)
+                system_energy = system_energy-dot_product(sx,sx_right)-dot_product(sx,sx_down)
+            end do
+        end do
+        system_energy = system_energy/VOLUME
+    end function
 
-function random_integer(i)
-    real r
-    integer i, random_integer
-    call random_number(r)
-    random_integer = ceiling(i*r)
-end function
+    function is_bond(sx, sy, w)
+        logical :: is_bond
+        real(8), dimension(3) :: sx, sy, w
+        real(8) :: delta
+        delta = -dot_product(wolff_reflection(sx, w), sy)+dot_product(sx, sy)
+        if (random()<=1-exp(min(0.,-beta*delta))) then
+            is_bond = .true.
+        else
+            is_bond = .false.
+        end if
+    end function
 
-function linspace(a, b, n)
-    integer n, i
-    real(8), dimension(n) :: linspace
-    real(8) a, b
-    do i=1, n
-        linspace(i) = a+(b-a)*(i-1)/(n-1)
-    end do
-end function
+    subroutine join(group, label1, label2, largest_label)
+        integer, dimension(LENGTH,LENGTH) :: group
+        integer :: label1, label2, label_min, label_max
+        if (label1/=label2) then
+            if (label1<label2) then
+                label_max = label2
+                label_min = label1
+            else
+                label_max = label1
+                label_min = label2
+            end if
+            do i=1, LENGTH
+                do j=1, LENGTH
+                    if (group(i,j)==label_max) then
+                        group(i,j) = label_min
+                    else if (group(i,j)>label_max) then
+                        group(i,j) = group(i,j)-1
+                    end if
+                end do
+            end do
+            largest_label = largest_label-1
+        end if
+    end subroutine
+
+    function char_to_int(i)
+        character(20) :: style, char_to_int
+        integer :: i
+        if (i<10) then
+            style = "(I1)"
+        else
+            style = "(I2)"
+        end if
+        write(char_to_int, style) i
+    end function
+
+    function random_integer(i)
+        real r
+        integer i, random_integer
+        call random_number(r)
+        random_integer = ceiling(i*r)
+    end function
+
+    function linspace(a, b, n)
+        integer n, i
+        real(8), dimension(n) :: linspace
+        real(8) a, b
+        do i=1, n
+            linspace(i) = a+(b-a)*(i-1)/(n-1)
+        end do
+    end function
 end module
--- main.f90
+++ main.f90
@@ -1,72 +1,71 @@
 program main
-use algorithm
-use functions
-use m_progress_bar
-real(8), allocatable, dimension(:,:,:) :: s
-real(8), allocatable, dimension(:,:) ::  Smp, medSmp
-real(8), allocatable, dimension(:) :: interval
-real(8) :: medJK, varJK, startTemp, endTemp
-integer :: N, thermalization, packages, sizeJk, TQ, j,k
-character(60) path
-LENGTH = 8
-VOLUME = LENGTH*LENGTH
-thermalization = 30
-startTemp = 4.
-endTemp = 0. 
-packages = 100
-N = 1e5
-TQ = 80
+    use algorithm
+    use functions
+    use m_progress_bar
+    real(8), allocatable, dimension(:,:,:) :: s
+    real(8), allocatable, dimension(:,:) ::  Smp, medSmp
+    real(8), allocatable, dimension(:) :: interval
+    real(8) :: medJK, varJK, startTemp, endTemp
+    integer :: N, thermalization, packages, sizeJk, TQ, j,k
+    character(60) path
+    LENGTH = 8
+    VOLUME = LENGTH*LENGTH
+    thermalization = 30
+    startTemp = 4.
+    endTemp = 0. 
+    packages = 100
+    N = 1e6
+    TQ = 20
 
-path = "output/susceptibility/80.tsv"
-open(unit=1, file=path)
-allocate(s(LENGTH,LENGTH,3))
-allocate(interval(TQ), Smp(TQ,packages), medSmp(TQ,packages))
-write(1, "((A4),*(A25))") 'TQ', 'Temp', 'susceptibility', 'error'
+    path = "output/susceptibility/10.tsv"
+    open(unit=1, file=path)
+    allocate(s(LENGTH,LENGTH,3))
+    allocate(interval(TQ), Smp(TQ,packages), medSmp(TQ,packages))
+    write(1, "((A4),*(A25))") 'TQ', 'Temp', 'susceptibility', 'error'
 
-sizeJk = N/packages
-Smp(:,:) = 0
-medSmp(:,:) = 0
-do i=1, packages
-    do j=1, sizeJk
-        interval = linspace(startTemp, endTemp, TQ)
-        call hot_start(s)
-        beta = 1/startTemp
-        do k=1, thermalization
-            call cluster(s)
+    sizeJk = N/packages
+    Smp(:,:) = 0
+    medSmp(:,:) = 0
+    do i=1, packages
+        do j=1, sizeJk
+            interval = linspace(startTemp, endTemp, TQ)
+            call hot_start(s)
+            beta = 1/startTemp
+            do k=1, thermalization
+                call cluster(s)
+            end do
+
+            do k=1, TQ
+                beta = 1/interval(k)
+                call metropolis(s)
+                Smp(k,i) = Smp(k,i)+system_charge(s)**2
+            end do
         end do
-
         do k=1, TQ
-            beta = 1/interval(k)
-            call metropolis(s)
-            Smp(k,i) = Smp(k,i)+system_charge(s)**2
+            Smp(k,i) = Smp(k,i)/sizeJk
         end do
+        call progress_bar(i, packages)
     end do
+
     do k=1, TQ
-        Smp(k,i) = Smp(k,i)/sizeJk
-    end do
-    call progress_bar(i, packages)
-end do
-
-do k=1, TQ
-    medJK = 0
-    varJK = 0
-    do i=1, packages
-        do j=1, packages
-            if (i/=j) then
-                medSmp(k,i) = medSmp(k,i)+Smp(k,j)
-            end if
+        medJK = 0
+        varJK = 0
+        do i=1, packages
+            do j=1, packages
+                if (i/=j) then
+                    medSmp(k,i) = medSmp(k,i)+Smp(k,j)
+                end if
+            end do
+            medSmp(k,i) = medSmp(k,i)/(packages-1)
+            medJK = medJK+medSmp(k,i)
         end do
-        medSmp(k,i) = medSmp(k,i)/(packages-1)
-        medJK = medJK+medSmp(k,i)
+        medJK = medJK/packages
+
+        do i=1, packages
+            varJK = varJK+(medSmp(k,i)-medJK)**2
+        end do
+        varJK = varJK*(packages-1)
+
+        write(1, "((I4),*(f25.16))") k, interval(k), medJK, sqrt(varJK/packages)
     end do
-    medJK = medJK/packages
-
-    do i=1, packages
-        varJK = varJK+(medSmp(k,i)-medJK)**2
-    end do
-    varJK = varJK*(packages-1)
-
-    write(1, "((I4),*(f25.16))") k, interval(k), medJK, sqrt(varJK/packages)
-end do
-
 end program
