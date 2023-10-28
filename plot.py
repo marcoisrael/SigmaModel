2ed17d0a2bd78494626f51959e25ac504c6247dc
marcoisrael <marco_israel@ciencias.unam.mx>
2023-10-28 09:06:12 -0600

    data upload

--- plot.py
+++ plot.py
@@ -1,9 +1,10 @@
-#!/bin/python3
+#!/usr/bin/python3
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 folder = "output/cooling/plot"
-algs = ['lexic_metropolis','random_metropolis','lexic_glauber', 'random_glauber','single_cluster']
+algs = ['lexic_metropolis','random_metropolis','lexic_glauber',
+		 'random_glauber','single_cluster']
 words = {'lexic':'lexicographical','random':'random','single':'single'}
 def update(alg):
 	if alg=='single_cluster':
@@ -15,7 +16,6 @@ if not os.path.isdir(folder):
 for alg in algs:
 	a, n = alg.split('_')
 	fig, ax = plt.subplots()
-	# ax.set_title(f'100 cluster termalization and 4 to 0 {words[a]} {n} cooling '+r'$V=64$',fontsize=10)
 	ax.set_xlabel(update(alg),fontsize=14)
 	ax.set_ylabel(r'$\left<Q^2\right>$',fontsize=14)
 	for i in [10,20,30,40]:
