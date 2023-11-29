20035a700fda42ffe0af67144b28a7511c308b07
Marco Israel Rodriguez Cornejo ICN <marcoisrael@xook.lamod.unam.mx>
2023-11-29 11:29:58 -0600

    res

--- run_job1.sh
+++ run_job1.sh
@@ -3,11 +3,11 @@
 #SBATCH --output=logs/SigmaModelLM_%j.out
 #SBATCH --nodes=1
 #SBATCH --mem-per-cpu=100mb
-#SBATCH --time=8:00:00
-#SBATCH --array=4-16:4
+#SBATCH --time=1:00:00
+#SBATCH --array=3-15:3
 module load lamod/gcc/12.2
 echo $USER;hostname;date
 N="1e4"
 alg="lexic,metropolis"
-name="v64x64/4p0-0p5"
-./sigmaModel -cool "4,0.5,${SLURM_ARRAY_TASK_ID}" -s "$N" -alg "$alg" -n "$name"
+name="v64x64/4p0-0p0"
+./sigmaModel -cool "4,0,${SLURM_ARRAY_TASK_ID}" -s "$N" -alg "$alg" -n "$name"
