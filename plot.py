1610e234ba99b7f481833f8f55ec434bbdd0698e
Israel <marco_israel@ciencias.unam.mx>
2023-12-06 20:25:12 -0600

    fix

--- plot.py
+++ plot.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 from loaddata import *
 startTemp = 4
-endTemp = 1.5
-
-path = f"output/cooling/v64x64/{tempName(startTemp,endTemp)}"
-dest = f"output/plot/v64x64/{tempName(startTemp,endTemp)}"
-dt = loadData("Multi", "Cluster", startTemp, endTemp, path, dest)
-dt.plot([3,6,9,12,15])
-for method in ["Random", "Lexic"]:
-    for alg in  ["Glauber", "Metropolis"]:
-        dt = loadData(method, alg, 4, 0, path, dest)
-        dt.plot([3,6,9,12,15])
+for endTemp in [0,.5,1,1.5,2,2.5,3]:
+    path = f"output/cooling/v64x64/{tempName(startTemp,endTemp)}"
+    dest = f"output/plot/v64x64/{tempName(startTemp,endTemp)}"
+    dt = loadData("Multi", "Cluster", startTemp, endTemp, path, dest)
+    dt.plot([3,6,9,12,15])
+    for method in ["Random", "Lexic"]:
+        for alg in  ["Glauber", "Metropolis"]:
+            dt = loadData(method, alg, 4, 0, path, dest)
+            dt.plot([3,6,9,12,15])
