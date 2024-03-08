9ed9eedf2d8c77267c0dda2daa67eabafa055cb2
marcoisrael <marco_israel@ciencias.unam.mx>
2024-03-08 14:19:45 -0600

    fix

--- run_job1.sh
+++ run_job1.sh
@@ -4,10 +4,6 @@
 #SBATCH --nodes=1
 #SBATCH --mem-per-cpu=100mb
 #SBATCH --time=3:00:00
-module load lamod/gcc/12.2
 echo $USER;hostname;date
-una hamburgesa por favor
 N="1e6"
 ./sigmaModel -t "$1" -s "$N" -alg "$2"
-cambio xd
-otro cambio
