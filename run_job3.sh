da1f0b593c94e4183e13f1333a6f917923cef45f
marcoisrael <marco_israel@ciencias.unam.mx>
2024-03-14 14:46:44 -0600

    update

---
+++ run_job3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name=SigmaModel
+#SBATCH --output=logs/SigmaModel_%j.out
+#SBATCH --nodes=1
+#SBATCH --mem-per-cpu=100mb
+#SBATCH --time=6:00:00
+echo $USER;hostname;date
+N="1e6"
+./sigmaModel -t "$1" -s "$N" -alg "$2" -m
