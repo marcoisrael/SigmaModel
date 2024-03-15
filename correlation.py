bd54f0a2f574a48498340df0f39a27661f0fda5a
marcoisrael <marco_israel@ciencias.unam.mx>
2024-03-14 19:30:32 -0600

    update

--- correlation.py
+++ correlation.py
@@ -1,62 +1,100 @@
 #!/usr/bin/python3
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-import os	
-alg_list = ["lexic_metropolis", "lexic_glauber", "random_metropolis", "random_glauber"]
-tmax = 30
-for temp in [4]:
-	for alg in alg_list:
-		data = pd.read_csv(f"output/thermalized/{temp}/{alg}.csv")
-		ac = (data*data).mean()-data.mean()**2
-		for t in np.arange(1,tmax):
-			ac = pd.concat([ac,(data*data.shift(t)).dropna().mean()-data.mean()**2],axis=1)
-		point = 3
-		ac = ac.transpose()
-		ac = ac.iloc[point:]
-		t = np.arange(point,tmax)
-		fig = plt.figure(dpi=140,figsize=(16,9))
-		gs = fig.add_gridspec(2, 2,  width_ratios=(1,1))
-		def f(x, a, b, c):
-			return c+a/(x+b)
-		x = np.linspace(point,tmax)
-		opt, cov = curve_fit(f, t, ac["H/V"])
-		ax1 = fig.add_subplot(gs[0,0])
-		ax1.plot(x,f(x, *opt))
-		ax1.plot(t, ac["H/V"],color="red", label=f"$H/V$", ls="",marker=".")
-		stats = "".join([r"$c(t)=k+c_0/(\tau + t)$","\n", r"$\tau =$",f"{opt[1].round(4)}"])
-		bbox = dict(boxstyle='round', fc='blanchedalmond', ec='orange', alpha=0.5)
-		ax1.text(0.95, 0.8, stats, fontsize=14, bbox=bbox,transform=ax1.transAxes, horizontalalignment='right')
-		ax1.set_xlabel("t", fontsize=15)
-		ax1.set_ylabel(r"$C_{E,E}(t)$",fontsize=15)
-		ax1.set_title("Energy density",fontsize=15)
+import matplotlib.pyplot as plt
 
-		opt, cov = curve_fit(f, t, ac["chi_t"])
-		ax2 = fig.add_subplot(gs[0,1])
-		ax2.plot(x,f(x, *opt))
-		ax2.plot(t, ac["chi_t"],color="purple", label=f"$\chi_t$", ls="", marker=".")
-		stats = "".join([r"$c(t)=k+c_0/(\tau + t)$","\n", r"$\tau =$",f"{opt[1].round(4)}"])
-		bbox = dict(boxstyle='round', fc='blanchedalmond', ec='orange', alpha=0.5)
-		ax2.text(0.95, 0.8, stats, fontsize=14, bbox=bbox,transform=ax2.transAxes, horizontalalignment='right')
-		ax2.set_xlabel("t",fontsize=15)
-		ax2.set_ylabel(r"$C_{Q,Q}(t)$",fontsize=15)
-		ax2.set_title("Topological charge",fontsize=15)
+def f(x, tau, k):
+	return k*np.exp(-x/tau)
 
-		opt, cov = curve_fit(f, t, ac["chi_m"])
-		ax3 = fig.add_subplot(gs[1,0])
-		ax3.plot(x,f(x, *opt))
-		ax3.plot(t, ac["chi_m"],color="orange", label=f"$\chi_m$",ls="", marker=".")
-		stats = "".join([r"$c(t)=k+c_0/(\tau + t)$","\n", r"$\tau =$",f"{opt[1].round(4)}"])
-		bbox = dict(boxstyle='round', fc='blanchedalmond', ec='orange', alpha=0.5)
-		ax3.text(0.95, 0.8, stats, fontsize=14, bbox=bbox,transform=ax3.transAxes, horizontalalignment='right')
-		ax3.set_xlabel("t",fontsize=15)
-		ax3.set_ylabel(r"$C_{M,M}(t)$",fontsize=15)
-		ax3.set_title("Magnetization",fontsize=15)
-		title = " ".join(alg.split("_"))
-		fig.suptitle(title,fontsize=16)
-		if not os.path.isdir(f"output/plot/thermalized/{temp}"):
-			os.makedirs(f"output/plot/thermalized/{temp}")
-		fig.savefig(f"output/plot/thermalized/{temp}/{alg}.png")
-		plt.close(fig) 
-		print(f"{alg}, Temp = {temp} done.")
+def f2(x, tau, k, c):
+	return c+k/(tau+x)
+def correlation(x, t):
+	mean = x.mean()
+	n = x.size-t
+	if t==0:
+		c = (x-mean)*(x-mean)
+	else:
+		c = (x[0:-t]-mean)*(x[t:]-mean)
+
+	s = np.sqrt((c*c).mean()-c.mean()**2)
+	serr = s/np.sqrt(n)
+	return t, c.mean(), serr
+
+def fix(x,dx):
+	before, after = str(dx).split('.')
+	i = 0
+	for d in after:
+		i +=1
+		if  d != "0":
+			val=  round(float(d+"."+after[i:]))
+			break
+	y = round(x,i)
+	if i>len(str(y).split('.')[1]):
+		y = f"{y}{'0'*(i-len(str(y).split('.')[1]))}"
+	return f"{y}({val})"
+
+def fit(data, t_min,t_max, sep):
+	X = []
+	for t in np.arange(t_min, t_max, sep):
+		X.append(correlation(data,t))
+
+	X = np.array(X).transpose()
+	opt, cov = curve_fit(f, X[0], X[1], p0=[1, 0])
+
+	chi2_by_dog = np.sum(((X[1]-f(X[0],*opt))/X[2])**2)/(X[0].size-opt.size)
+	return opt[0], np.sqrt(cov[0,0]/X[0].size), chi2_by_dog
+
+def fit2(X):
+	opt, cov = curve_fit(f2, X[0], X[1])
+
+	chi2_by_dog = np.sum(((X[1]-f2(X[0],*opt))/X[2])**2)/(X[0].size-opt.size)
+	return opt, np.sqrt(cov[0,0]/X[0].size), chi2_by_dog
+
+name = "record-0"
+alg = "lexic_metropolis"
+
+
+Q, M, E = [], [], []
+T = [1.5, 2, 2.5, 3, 3.5, 4]
+T_max = [200, 150, 100, 50, 25, 25]
+t_min, sep = 0, 1 
+for i in np.arange(len(T)):
+	t_max = T_max[i]
+	temp = T[i]
+	path = f"output/{name}/{temp}/{alg}.csv"
+	data = np.loadtxt(path, delimiter=",", skiprows=1).transpose()
+	tau, tauerr, chi2_by_dog = fit(data[0], t_min, t_max, sep)
+	E.append([temp, tau, tauerr, chi2_by_dog])
+
+	tau, tauerr, chi2_by_dog = fit(data[1], t_min, t_max, sep)
+	Q.append([temp, tau, tauerr, chi2_by_dog])
+
+	tau, tauerr, chi2_by_dog = fit(data[2], t_min, t_max, sep)
+	M.append([temp, tau, tauerr, chi2_by_dog])
+
+fig = plt.figure(dpi=140)
+ax = fig.add_subplot()
+
+setings = {
+	"Q":{"data":Q,"color":"red","label":r"$\tau_Q$"},
+	"E":{"data":E,"color":"gray","label":r"$\tau_E$"},
+	"M":{"data":M,"color":"cyan","label":r"$\tau_M$"}
+	}
+
+for s in setings:
+	st = setings[s]
+	X = np.array(st["data"]).transpose()
+	opt, err, chi2_by_dog = fit2(X)
+	print(chi2_by_dog)
+	x = np.linspace(1, 4, 200)
+	ax.plot(x, f2(x,*opt))
+	ax.errorbar(X[0], X[1], yerr=X[2], ls='', marker=".", label=st["label"]+"="+f"{fix(opt[0],err)}", color=st["color"])
+
+stats = "".join([r"$F(\tau)=C+k/(\alpha+\tau)$"])
+bbox = dict(boxstyle='round', fc='blanchedalmond', ec='orange', alpha=0.5)
+ax.text(0.95, 0.3, stats, fontsize=12, bbox=bbox,transform=ax.transAxes, horizontalalignment='right')
+ax.legend()
+ax.set_xlabel(r"$T$", fontsize=14)
+ax.set_ylabel(r"$\tau$", fontsize=14)
+ax.set_title(alg, fontsize=14)
+fig.savefig(f"output/{alg}.png")
--- plotCorrelation.py
+++ plotCorrelation.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import numpy as np
 from scipy.optimize import curve_fit
 import  matplotlib.pyplot as plt
@@ -13,10 +14,10 @@ def correlation(x, t):
 
 	s = np.sqrt((c*c).mean()-c.mean()**2)
 	serr = s/np.sqrt(n)
-	return c.mean(), serr
+	return t, c.mean(), serr
 
-def f(x, tau, c):
-	return c*np.exp(-x/tau)
+def f(x, tau, k):
+	return k*np.exp(-x/tau)
 
 def fix(x,dx):
 	before, after = str(dx).split('.')
@@ -31,20 +32,15 @@ def fix(x,dx):
 		y = f"{y}{'0'*(i-len(str(y).split('.')[1]))}"
 	return f"{y}({val})"
 
-def fit(data, t_min,t_max):
+def fit(data, t_min,t_max, sep):
 	X = []
-	T = np.arange(t_min, t_max)
-	n = t_max-t_min
-	for t in T:
+	for t in np.arange(t_min, t_max, sep):
 		X.append(correlation(data,t))
 
 	X = np.array(X).transpose()
+	opt, cov = curve_fit(f, X[0], X[1], p0=[1, 0])
 
-	opt, cov = curve_fit(f, T, X[0])
-
-	chi2_by_dog = np.sum(((X[0]-f(T,*opt))/X[1])**2)/(T.size-opt.size)
-	print(chi2_by_dog.round(4), fix(opt[0], np.sqrt(cov[0,0]/n)))
-	print(opt[0])
+	chi2_by_dog = np.sum(((X[1]-f(X[0],*opt))/X[2])**2)/(X[0].size-opt.size)
 	return X, opt, cov, chi2_by_dog
 
 def plot(X, st):
@@ -53,41 +49,47 @@ def plot(X, st):
 	ax.set_ylabel(st["ylabel"], fontsize=14)
 	ax.set_title(st["title"], fontsize=14)
 	x = np.linspace(t_min, t_max, 200)
-	T = np.arange(t_min, t_max)
-	ax.errorbar(T, X[0], yerr=X[1], ls="", marker=".")
+	ax.errorbar(X[0], X[1], yerr=X[2], ls="", marker=".")
 	ax.plot(x, f(x,*opt))
-	stats = "".join([r"$c(t)=k\exp(-t/\tau)$","\n", r"$\tau =$",f"{fix(opt[0], np.sqrt(cov[0,0]/T.size))}","\n",r"$\chi^2\left/dog\right. =" f"$ {chi2_by_dog.round(4)}"])
+	if False: 
+		stats = "".join([r"$c(t)=k\cdot\exp(-t/\tau)$","\n", r"$\tau =$",f"{fix(opt[0], np.sqrt(cov[0,0]/T.size))}","\n",r"$\chi^2\left/dog\right. =" f"$ {chi2_by_dog.round(4)}"])
+	else:
+		stats = "".join([r"$c(t)=k\cdot\exp(-t/\tau)$","\n", r"$\tau =$",f"{opt[0].round(4)}","\n",r"$\chi^2\left/dog\right. =" f"$ {chi2_by_dog.round(4)}"])
+
 	bbox = dict(boxstyle='round', fc='blanchedalmond', ec='orange', alpha=0.5)
 	ax.text(0.95, 0.6, stats, fontsize=12, bbox=bbox,transform=ax.transAxes, horizontalalignment='right')
 
-temp = 4
-Algs = ["lexic_metropolis", "random_metropolis", "lexic_glauber", "random_glauber"]
+name = "record-0"
+temp = 2.5
+Algs = ["lexic_metropolis", "random_metropolis", "lexic_glauber", "random_glauber", "multi_cluster"]
+# Algs = ["multi_cluster"]
+t_min, t_max, sep = 0, 200, 1
 
-if not os.path.isdir(f"output/plot/thermalized/{temp}"):
-	os.makedirs(f"output/plot/thermalized/{temp}")
+if not os.path.isdir(f"output/plot/{name}/{temp}"):
+	os.makedirs(f"output/plot/{name}/{temp}")
 
 for alg in Algs:
-	path = f"output/thermalized/{temp}/{alg}.csv"
+	path = f"output/{name}/{temp}/{alg}.csv"
 	data = np.loadtxt(path, delimiter=",", skiprows=1).transpose()
 
 	fig = plt.figure(figsize=(16,9))
 	gs = fig.add_gridspec(2, 2)	
 
-	t_min, t_max = 0, 30
 	st = {"xlabel":r"$t$", "ylabel":r"$C_{E,E}(t)$", "title":"Energy density", "gsx":1, "gsy":0}
-	X, opt, cov, chi2_by_dog = fit(data[0], t_min, t_max)
+	X, opt, cov, chi2_by_dog = fit(data[0], t_min, t_max, sep)
 	plot(X, st)
 
 	st = {"xlabel":r"$t$", "ylabel":r"$C_{Q,Q}(t)$", "title":"Topological charge", "gsx":0, "gsy":0}
-	X, opt, cov, chi2_by_dog = fit(data[1], t_min, t_max)
+	X, opt, cov, chi2_by_dog = fit(data[1], t_min, t_max, sep)
 	plot(X, st)
 
 	st = {"xlabel":r"$t$", "ylabel":r"$C_{M,M}(t)$", "title":"Magnetization density", "gsx":0, "gsy":1}
-	X, opt, cov, chi2_by_dog = fit(data[2], t_min, t_max)
+	X, opt, cov, chi2_by_dog = fit(data[2], t_min, t_max, sep)
 	plot(X, st)
 	# plt.tight_layout()
 	fig.suptitle(alg,fontsize=16)
 	plt.subplots_adjust(hspace=0.3, wspace=0.2)
-	name = f"output/plot/thermalized/{temp}/{alg}.png"
-	fig.savefig(name)
-	del fig
+	plot_name = f"output/plot/{name}/{temp}/{alg}.png"
+	fig.savefig(plot_name)
+	print(plot_name, "finished")
+	del fig, data
