fb0deaef8b853c0381f0ba190d84554b05045532
marcoisrael <marco_israel@ciencias.unam.mx>
2024-05-31 15:36:26 -0600

    update

--- run_job1.sh
+++ run_job1.sh
@@ -5,9 +5,9 @@
 #SBATCH --partition=QuantPhysMC
 #SBATCH --mem-per-cpu=100mb
 #SBATCH --time=6:00:00
-#SBATCH --array=1
+#SBATCH --array=0,1
 values="0.85 0.8 1.2 1.4 1.6 1.8"
 arr=($values)
 echo $USER;hostname;date
-echo "/sigmaModel -sp 2 -r -s 1e5 -alg lexic,metropolis -t ${arr[${SLURM_ARRAY_TASK_ID}]} -l "$1""
-./sigmaModel -sp 2 -r -s 1e5 -alg lexic,metropolis -t ${arr[${SLURM_ARRAY_TASK_ID}]} -l "$1"
+echo "/sigmaModel -r -s 1e5 -alg lexic,metropolis -t ${arr[${SLURM_ARRAY_TASK_ID}]} -l "$1""
+./sigmaModel -r -s 1e5 -alg lexic,metropolis -t ${arr[${SLURM_ARRAY_TASK_ID}]} -l "$1"
