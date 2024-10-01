d39fe0ff02a5d132e7c0c5d1d5677b81e22d7ac5
marcoisrael <marco_israel@ciencias.unam.mx>
2024-10-01 00:36:40 -0600

    hot fix

--- plot_cooling.py
+++ plot_cooling.py
@@ -5,27 +5,28 @@ import matplotlib.pyplot as plt
 from plotClass import *
 from scipy import interpolate
 
+
+
 alg = "lexic_metropolis"
 params = {
-			"charge":{"index":2, "ylabel":r"$\chi_{t_f}$"}, 
-			"energy":{"index":4, "ylabel":r"$E_f$"}, 
-			"magnet":{"index":6, "ylabel":r"$\chi_{m_f}$"}
+			"charge":{"index":2, "ylabel":r"$\chi_{t}$"}, 
+			"energy":{"index":4, "ylabel":r"$E$"}, 
+			"magnet":{"index":6, "ylabel":r"$\chi_{m}$"}
 			}
-
+colors_list = ["red", "green", "blue", "purple"]
 markers = dict(zip([4,8,16,20],["2",".","x","+"]))
 for obs in ["charge", "energy", "magnet"]:
 	ob = params[obs]["index"]
 	path = "output/cooling/FastCooling/4-0"
 	indexes = [4,8,16,20]
 	fig, ax = plt.subplots()
-	for i in indexes:
+	for i, color in zip(indexes, colors_list):
 		data = np.loadtxt(f"{path}/{alg} {i}.csv", delimiter=",", skiprows=1).transpose()
-		ax.errorbar(data[1], data[ob], data[ob+1], marker=markers[i], color="black", ls="", label=r"$\tau_{cool}$"+f"={i}")
+		ax.errorbar(data[1], data[ob], data[ob+1], marker=markers[i], color=color, ls="", label=f"$\\tau_{{\\mathrm{{cool}}}}={i}$")
 		x=np.linspace(0, 4)
 		f = interpolate.interp1d(data[1], data[ob], kind="cubic")
-		ax.plot(x, f(x), color="black", linewidth=0.8)
+		ax.plot(x, f(x), color=color, linewidth=0.8)
 	ax.legend()
-	ax.grid(True)
 	ax.set_xlabel(r"$T$", fontsize=20)
 	ax.set_ylabel(params[obs]["ylabel"], fontsize=20)
-	plt.savefig(f"output/plot/cooling/{obs}_{alg}.svg", format="svg")
+	plt.savefig(f"output/plot/cooling/{obs}_{alg}.pdf", format="pdf",bbox_inches="tight")
