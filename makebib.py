2a5651246cacaa7dad6b049bee572c4a08c7cce5
marcoisrael <marco_israel@ciencias.unam.mx>
2025-10-30 23:58:36 -0600

    correccoiones

--- makebib.py
+++ makebib.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
 import re
 import os
+
 HOME = "/home/israel"
-if os.path.isfile(f"{HOME}/citations/citations.bib"):
-    os.system(f"cp {HOME}/citations/citations.bib citations.bib")
-    #os.system(f"rm {HOME}/Descargas/citations.bib")
+SRC = f"{HOME}/citations/citations.bib"
+DST = "citations.bib"
+
+# --- Copiar archivo si existe ---
+if os.path.isfile(SRC):
+    os.system(f"cp {SRC} {DST}")
     print("archivo actualizado")
-file = open("citations.bib").read()
+
+# --- Leer contenido ---
+with open(DST, "r", encoding="utf-8") as f:
+    file = f.read()
+
+# --- LIMPIEZA DE ESCAPES LaTeX ---
+# Convierte "\$" -> "$" y "\_" -> "_"
+file = re.sub(r"\\\$", "$", file)
+file = re.sub(r"\\_", "_", file)
+file = re.sub(r"\{\\textasciicircum\}", "^", file)
+
+# --- Procesamiento habitual ---
 cite_list = file.split("@")
 outlines = []
+
 for cite in cite_list[1:]:
     if re.search("article", cite):
         regexp = "@article|title =|volume =|doi =|journal =|author =|year ="
@@ -19,16 +35,19 @@ for cite in cite_list[1:]:
         for line in f"@{cite}".splitlines():
             if re.search(regexp, line):
                 outlines.append(f"{line}\n")
-    if re.search("book", cite):
+
+    elif re.search("book", cite):
         cite = cite.replace("book", "article")
         cite = cite.replace("publisher", "journal")
         regexp = "@article|\ttitle =|doi =|journal =|author =|year =|pages ="
         for line in f"@{cite}".splitlines():
-            if re.search("@book|@incollection", line):
-                outlines.append(f"{line}\n".replace)
             if re.search(regexp, line):
                 outlines.append(f"{line}\n")
+
     outlines.append("}\n\n")
-outfile = open("bibliography.bib", "w")
-outfile.writelines(outlines)
+
+# --- Guardar salida ---
+with open("bibliography.bib", "w", encoding="utf-8") as out:
+    out.writelines(outlines)
+
 print("archivo generado")
